0f12be4cd9d931b90ffcdb70d0a5fa6c35bf8b28
gyubeom-j <maius.triginta@gmail.com>
2026-09-10 13:00:58 +0900

    feat(ssmctl): v0.1.0-rc.2 Homebrew 패키지 배포

--- Formula/ssmctl.rb
+++ Formula/ssmctl.rb
@@ -2,12 +2,12 @@
 class Ssmctl < Formula
   desc "AWS Systems Manager 세션 접속 도구"
   homepage "https://github.com/gyubeom-j/ssmctl"
-  version "0.1.0-rc.1"
+  version "0.1.0-rc.2"
 
   bottle do
     root_url "https://ghcr.io/v2/gyubeom-j/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3c92f6a1551ce476312062858a453edd209c104a3e971292384635d23d65670c"
-    sha256 cellar: :any_skip_relocation, sequoia:       "54b8db1e34756fb7af1e7ba95b21dfb9c6353e5fbe3e2a9fc104418d358c84ec"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5b5f0f930ac98f411f44a889632645bb7936e515816df36dbe327510650729f1"
+    sha256 cellar: :any_skip_relocation, sequoia:       "8cf544eaa77bf3d8a7f59d217b89f06a6749d2a986b8e58885be91bd5be91874"
   end
 
   # 이 URL은 bottle 제작용 입력입니다. 설치 사용자는 bottle을 받습니다.
@@ -15,12 +15,12 @@ class Ssmctl < Formula
     depends_on macos: :sequoia
 
     on_arm do
-      url "https://github.com/gyubeom-j/ssmctl/releases/download/v0.1.0-rc.1/ssmctl_v0.1.0-rc.1_darwin_arm64.tar.gz"
-      sha256 "1fb83929e87ff1b85f369aaece51ded95f09476332c919c4112edcd880ad4b38"
+      url "https://github.com/gyubeom-j/ssmctl/releases/download/v0.1.0-rc.2/ssmctl_v0.1.0-rc.2_darwin_arm64.tar.gz"
+      sha256 "039c06efa93c285a1676541bada4a2b200b1e9e5de2186f3aaf2136ef9d99c99"
     end
     on_intel do
-      url "https://github.com/gyubeom-j/ssmctl/releases/download/v0.1.0-rc.1/ssmctl_v0.1.0-rc.1_darwin_amd64.tar.gz"
-      sha256 "1db70138cca7b43adee1c323816849583f27396e5d8c69295465f440dc36202e"
+      url "https://github.com/gyubeom-j/ssmctl/releases/download/v0.1.0-rc.2/ssmctl_v0.1.0-rc.2_darwin_amd64.tar.gz"
+      sha256 "9ad296116409597d2fd1cf709896bf9828752ec67700bb882fd741013cf4a746"
     end
   end
 
@@ -42,7 +42,7 @@ class Ssmctl < Formula
   end
 
   test do
-    assert_match "v0.1.0-rc.1", shell_output("#{bin}/ssmctl version")
+    assert_match "v0.1.0-rc.2", shell_output("#{bin}/ssmctl version")
     assert_match "ssmctl", shell_output("#{bin}/ssmctl --help")
     assert_path_exists prefix/"docs/architecture.md"
     assert_path_exists prefix/"licenses/go/LICENSE"
